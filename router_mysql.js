cee83f7dc143b0e52120785c2fb2dc984ff0d4e9
Simon Walz <contact@simonwalz.de>
2015-04-15 18:33:41 +0200

    mysql pool

--- router_mysql.js
+++ router_mysql.js
@@ -1,28 +1,53 @@
-var mysql      = require('mysql');
+
+// http://codeforgeek.com/2015/01/nodejs-mysql-tutorial/
+
+var mysql = require('mysql');
 
 exports.init = function(router, basename, mysql_config) {
-	exports.connection = mysql.createConnection(mysql_config);
+	mysql_config.connectionLimit = 100; //important
+	mysql_config.debug = false;
 
-	exports.connection.connect();
+	exports.pool = mysql.createPool(mysql_config);
 
 	var mid = {};
-	exports.connection.query('SELECT m.id, CONCAT(s.Name, "/", m.Name) AS node FROM Measurement AS m LEFT JOIN Station AS s ON m.Station_id = s.id;', function(err, rows, fields) {
-		if (!err) {
-			for(var i=0;i<rows.length;i++) {
-				mid[rows[i].node] = rows[i].id;
-
-				var ref = router.register(basename + "/" + rows[i].node, {"to": router.dests.mysql, "id": rows[i].id});
-			}
-		} else {
-			console.log('Error while performing Query.');
+	exports.pool.getConnection(function(err, connection) {
+		if (err) {
+			connection.release();
+			console.warn("Error in connection database");
+			return;
 		}
+
+		connection.query('SELECT m.id, CONCAT(s.Name, "/", m.Name) AS node FROM Measurement AS m LEFT JOIN Station AS s ON m.Station_id = s.id;', function(err, rows, fields) {
+			if (!err) {
+				for(var i=0;i<rows.length;i++) {
+					mid[rows[i].node] = rows[i].id;
+
+					var ref = router.register(basename + "/" + rows[i].node, {"to": router.dests.mysql, "id": rows[i].id});
+				}
+			} else {
+				console.log('Error while performing Query: ' + err);
+			}
+		});
+		connection.on("error", function(err) {
+			console.warn("Error in connection database: Error: " + err);
+		});
 	});
 
 
 	router.dests.mysql = function(id, name, time, value) {
-//		if (mid.hasOwnProperty(id)) {
-//			exports.connection.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + mid[id] + ', ' + time + ', ' + value + ')');
-//		}
-		exports.connection.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + id + ', ' + time + ', ' + value + ')');
+		exports.pool.getConnection(function(err, connection) {
+			if (err) {
+				connection.release();
+				console.warn("Error in connection database");
+				return;
+			}
+
+			connection.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + id + ', ' + time + ', ' + value + ')', function(err, rows) {
+				connection.release();
+			});
+			connection.on("error", function(err) {
+				console.warn("Error in connection database: Error: " + err);
+			});
+		});
 	};
 };
