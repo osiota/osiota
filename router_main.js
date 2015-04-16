3e2511fca0a831132d9a657a4f16ad247b01b58e
Simon Walz <contact@simonwalz.de>
2015-04-16 17:49:20 +0200

    minor changes

--- router_main.js
+++ router_main.js
@@ -12,8 +12,8 @@ var router = require('./router.js');
 require('./router_mysql.js').init(router, "/mysql", mysql_config);
 require('./router_console_out.js').init(router, "/console");
 require('./router_websockets.js').init(router, "/ws", 8080);
-require('./router_console_in.js').init(router, "/in);
-//require('./router_childprocess.js').init(router, "/ethercat", "../ethercat_bridge/main", "");
+require('./router_console_in.js').init(router, "");
+require('./router_childprocess.js').init(router, "/ethercat", "../ethercat_bridge/main", ["../ethercat_bridge/config.csv"]);
 
 
 router.route('/Klemme_1/Wert_2', 0, 230);
@@ -27,9 +27,9 @@ router.register('/Klemme_1/Wert_1', {"to": function(id, name, time, v) {
 }, "id": "-"});
 
 router.register("/Klemme_1/Wert_1",
-	{"to": exports.dests.console, "id": "Wert 1", "f": function(v) {return v * 1000; }}
+	{"to": router.dests.console, "id": "Wert 1", "f": function(v) {return v * 1000; }}
 );
 router.register("/Engel/Energie_P1",
-	{"to": exports.dests.console, "id": "P", "f": function(v) { return v * 1000; }}
+	{"to": router.dests.console, "id": "P", "f": function(v) { return v * 1000; }}
 );
 
