81f10f4e3763a6feb688712985459fe6c1e6e9f9
Simon Walz <contact@simonwalz.de>
2015-04-21 15:24:16 +0200

    Router: Console out

--- router_console_out.js
+++ router_console_out.js
@@ -1,6 +1,6 @@
 
 exports.init = function(router, basename) {
-	router.console = function(id, name, time, value) {
+	router.dests.console = function(id, name, time, value) {
 		console.log(id + ": " + name + " [" + time + "]:\t" + value);
 	};
 }
