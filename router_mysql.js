33c0148479cd9098ce159384dc1989c45494a0c9
Simon Walz <contact@simonwalz.de>
2015-04-21 19:12:54 +0200

    Router, MySQL: using own query function

--- router_mysql.js
+++ router_mysql.js
@@ -7,47 +7,50 @@ exports.init = function(router, basename, mysql_config) {
 	mysql_config.connectionLimit = 100; //important
 	mysql_config.debug = false;
 
+	// Create Mysql Pool Connection:
 	exports.pool = mysql.createPool(mysql_config);
 
-	var mid = {};
-	exports.pool.getConnection(function(err, connection) {
-		if (err) {
-			connection.release();
-			console.warn("Error in connection database");
-			return;
-		}
+	// Send a query to keep Connection alive (every 30 seconds)
+	setInterval(function() {
+		exports.query('SELECT 1');
+	}, 30000)
 
-		connection.query('SELECT m.id, CONCAT(s.Name, "/", m.Name) AS node FROM Measurement AS m LEFT JOIN Station AS s ON m.Station_id = s.id;', function(err, rows, fields) {
-			if (!err) {
-				for(var i=0;i<rows.length;i++) {
-					mid[rows[i].node] = rows[i].id;
-
-					var ref = router.register(basename + "/" + rows[i].node, {"to": router.dests.mysql, "id": rows[i].id});
-				}
-			} else {
-				console.log('Error while performing Query: ' + err);
-			}
-		});
-		connection.on("error", function(err) {
-			console.warn("Error in connection database: Error: " + err);
-		});
-	});
-
-
-	router.dests.mysql = function(id, name, time, value) {
+	// a simple query function using Mysql Pool Connections
+	exports.query = function(query, callback) {
 		exports.pool.getConnection(function(err, connection) {
 			if (err) {
 				connection.release();
 				console.warn("Error in connection database");
 				return;
 			}
-
-			connection.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + id + ', ' + time + ', ' + value + ')', function(err, rows) {
-				connection.release();
-			});
 			connection.on("error", function(err) {
 				console.warn("Error in connection database: Error: " + err);
 			});
-		});
+			connection.query(query, function(err, rows, fields) {
+				connection.release();
+				if (err) {
+					console.log('Error while performing Query: ' + err);
+				} else {
+					if (typeof callback === "function")
+						callback(rows, fields);
+				}
+			});
+		};
+	}
+	// Register MySQL Destination:
+	router.dests.mysql = function(id, name, time, value) {
+		exports.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + id + ', ' + time + ', ' + value + ')');
 	};
+
+
+	// Get measurement names
+	var mid = {};
+	exports.query('SELECT m.id, CONCAT(s.Name, "/", m.Name) AS node FROM Measurement AS m LEFT JOIN Station AS s ON m.Station_id = s.id;', function(rows, fields) {
+		for(var i=0;i<rows.length;i++) {
+			mid[rows[i].node] = rows[i].id;
+
+			var rentry = router.register(basename + "/" + rows[i].node, router.dests.mysql, rows[i].id);
+		}
+	});
+
 };
