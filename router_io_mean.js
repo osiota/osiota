2af3fb1d5a631623f12790e2cec636b4c31509c0
Simon Walz <contact@simonwalz.de>
2015-04-30 17:25:31 +0200

    Router, IO Mean: converting inputs to numbers

--- router_io_mean.js
+++ router_io_mean.js
@@ -3,25 +3,27 @@
 exports.init = function(router, basename) {
 	
 	var meandata = {};
-	router.dests.mean = function(id, time, value, ref) {
+	router.dests.mean = function(id, time, value, name, obj) {
 		var timebase = 1;
-		if (ref.hasOwnProperty("timebase")) {
-			timebase = ref.timebase;
+		if (typeof obj === "number") {
+			timebase = obj;
+		} else if (typeof obj === "string" && obj.match(/^[0-9.]+$/)) {
+			timebase = 1*obj;
 		}
 		if (!meandata.hasOwnProperty(id)) {
 			meandata[id] = {"anz": 0, "sum": 0, "time": 0 };
 		}
 		if (meandata[id].time == Math.round(time / timebase)) {
 			meandata[id].anz++;
-			meandata[id].sum += value;
+			meandata[id].sum += (value*1);
 		} else {
 			if (meandata[id].anz != 0) {
 				var v = meandata[id].sum / meandata[id].anz;
-				router.route(id, time, v);
+				router.route(id, meandata[id].time, v);
 			}
 			meandata[id].time = Math.round(time / timebase);
-			meandata[id].sum = 0;
-			meandata[id].anz = 0;
+			meandata[id].sum = 1*value;
+			meandata[id].anz = 1;
 		}
 	};
 
