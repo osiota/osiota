fee098962fe3a1ba2e112955a20fd74ca7e15520
Simon Walz <contact@simonwalz.de>
2015-05-05 12:34:43 +0200

    Router, IO: Sum

---
+++ router_io_sum.js
@@ -0,0 +1,19 @@
+
+exports.init = function(router, basename) {
+	router.dests.sum = function(id, time, value, name, obj) {
+		if (typeof obj !== "object" || Object.prototype.toString.call(obj) !== '[object Array]') {
+			obj = [obj];
+		}
+		value *= 1;
+		for (var k=0;k<obj.length;k++) {
+			var node2_name = obj[k];
+			var node = r.get(node2_name);
+
+			if (node.hasOwnProperty("value") && node.value !== null) {
+				value += 1*node.value;
+			}
+		}
+		r.route(id, time, value);
+	};
+};
+
