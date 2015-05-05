a7bf5bbdcbe3fb23b9a5f7267fa6af103f495793
Simon Walz <contact@simonwalz.de>
2015-05-05 12:34:31 +0200

    Router, IO: Multiply: Multiply multiple nodes

--- router_io_multiply.js
+++ router_io_multiply.js
@@ -1,13 +1,19 @@
 
 exports.init = function(router, basename) {
 	router.dests.multiply = function(id, time, value, name, obj) {
-		var node2_name = obj;
-		var node = r.get(node2_name);
-		if (node.hasOwnProperty("value") && node.value !== null) {
-			value *= 1;
-			value *= 1*node.value;
-			r.route(id, time, value);
+		if (typeof obj !== "object" || Object.prototype.toString.call(obj) !== '[object Array]') {
+			obj = [obj];
 		}
+		value *= 1;
+		for (var k=0;k<obj.length;k++) {
+			var node2_name = obj[k];
+			var node = r.get(node2_name);
+
+			if (node.hasOwnProperty("value") && node.value !== null) {
+				value *= 1*node.value;
+			}
+		}
+		r.route(id, time, value);
 	};
 };
 
