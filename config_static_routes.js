6d06a786cc18e978acc130453a6cd2af440814d3
Simon Walz <contact@simonwalz.de>
2015-05-28 13:44:29 +0200

    Config, Static Routes

--- config_static_routes.js
+++ config_static_routes.js
@@ -1,4 +1,39 @@
 exports.static_routes = {
-	'/ethercat/CNC/Individualiser_current':'/mysql/CNC/Individualiser',
-	'/ethercat/CNC/Exhaust_current':'/mysql/CNC/Exhaust'
-};
+'/ethercat/DistributionA/Airflow':'/mysql/DistributionA/Air',
+'/ethercat/DistributionA/PLC':'/mysql/DistributionA/PLC',
+'/ethercat/DistributionB/Airflow':'/mysql/DistributionB/Air',
+'/ethercat/DistributionB/Belt':'/mysql/DistributionB/Belt',
+'/ethercat/DistributionB/Individualiser':'/mysql/DistributionB/Individualiser',
+'/ethercat/DistributionB/PLC':'/mysql/DistributionB/PLC',
+'/ethercat/TransportA/PLC':'/mysql/TransportA/PLC',
+'/ethercat/TransportA/Belt':'/mysql/TransportA/Belt',
+'/ethercat/TransportA/Airflow':'/mysql/TransportA/Air',
+'/ethercat/Assembling/PLC':'/mysql/Assembling/PLC',
+'/ethercat/Assembling/Airflow':'/mysql/Assembling/Air',
+'/ethercat/Assembling/Belt':'/mysql/Assembling/Belt',
+'/ethercat/Assembling/Individualiser':'/mysql/Assembling/Individualiser',
+'/ethercat/Press/Airflow':'/mysql/Press/Air',
+'/ethercat/Press/PLC':'/mysql/Press/PLC',
+'/ethercat/Switch/Airflow':'/mysql/Switch/Air',
+'/ethercat/Switch/Belt2':'/mysql/Switch/Belt2',
+'/ethercat/Switch/Belt1':'/mysql/Switch/Belt1',
+'/ethercat/Switch/PLC':'/mysql/Switch/PLC',
+'/ethercat/Furnace/PLC':'/mysql/Furnace/PLC',
+'/ethercat/Furnace/Furnace':'/mysql/Furnace/Furnace',
+'/ethercat/Furnace/Individualiser2':'/mysql/Furnace/Individualiser2',
+'/ethercat/Furnace/Individualiser1':'/mysql/Furnace/Individualiser1',
+'/ethercat/Furnace/Belt1':'/mysql/Furnace/Belt1',
+'/ethercat/Furnace/Belt2':'/mysql/Furnace/Belt2',
+'/ethercat/TransportB/PLC':'/mysql/TransportB/PLC',
+'/ethercat/TransportB/Belt':'/mysql/TransportB/Belt',
+'/ethercat/TransportB/Airflow':'/mysql/TransportB/Air',
+'/ethercat/CNC/Spindle':'/mysql/CNC/Spindle',
+'/ethercat/CNC/Exhaust':'/mysql/CNC/Exhaust',
+'/ethercat/CNC/Individualiser':'/mysql/CNC/Individualiser',
+'/ethercat/CNC/Belt':'/mysql/CNC/Belt',
+'/ethercat/CNC/Airflow':'/mysql/CNC/Air',
+'/ethercat/CNC/Position':'/mysql/CNC/Position',
+'/ethercat/CNC/PLC':'/mysql/CNC/PLC'
+}
+;
+
