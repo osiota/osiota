8edcc8e758e668621a3985040004f5a320476a54
Simon Walz <contact@simonwalz.de>
2015-05-29 16:03:57 +0200

    Router, IO: bias

---
+++ router_io_bias.js
@@ -0,0 +1,24 @@
+
+/* A module to substract a bias value */
+exports.init = function(router, basename) {
+	
+	var mindata = {};
+	router.dests.bias = function(id, time, value, name, obj) {
+		if (typeof value !== "number") {
+			return;
+		}
+		if (!mindata.hasOwnProperty(id)) {
+			if (value < 0.1) return;
+
+			mindata[id] = {"value": value };
+		}
+		if (mindata[id].value > value && value > 0.1) {
+			v = 0;
+			mindata[id].value = value;
+		} else {
+			v = value - mindata[id].value;
+		}
+		router.route(id, time, v);
+	};
+
+};
