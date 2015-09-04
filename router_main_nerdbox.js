aa53f451ad53bca8a2ce51ae65565e5f2286bfe6
Simon Walz <contact@simonwalz.de>
2015-09-04 10:37:54 +0200

    Router, Main: Nerdbox

---
+++ router_main_nerdbox.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+var Router = require('./router.js').router;
+var r = new Router();
+
+require('./router_console_out.js').init(r, "/console");
+require('./router_websockets.js').init(r, "", 8080);
+
+require('./router_io_function.js').init(r);
+require('./router_io_mean.js').init(r);
+require('./router_io_bias.js').init(r);
+require('./router_io_multiply.js').init(r);
+require('./router_io_sum.js').init(r);
+
+
