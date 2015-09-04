49eed539d7842e775567332983b4f234ed5f9239
Simon Walz <contact@simonwalz.de>
2015-09-04 10:41:00 +0200

    Router, Main: deleting root /ws for WebSocketServer

--- router_main.js
+++ router_main.js
@@ -12,7 +12,7 @@ var r = new Router();
 
 require('./router_mysql.js').init(r, "/mysql", mysql_config);
 require('./router_console_out.js').init(r, "/console");
-require('./router_websockets.js').init(r, "/ws", 8080);
+require('./router_websockets.js').init(r, "", 8080);
 require('./router_console_in.js').init(r, "");
 //require('./router_random_in.js').init(r, "/ethercat/Engel/Energie_P1", 20, 0, 100);
 require('./router_childprocess.js').init(r, "/ethercat", "../ethercat_bridge/main", ["../ethercat_bridge/config.csv"]);
--- router_main_pi53.js
+++ router_main_pi53.js
@@ -6,7 +6,7 @@ var r = new Router();
 require('./router_jsonconnect.js').init(r, "http://localhost/energy/get_sensors.php");
 
 require('./router_console_out.js').init(r, "/console");
-require('./router_websockets.js').init(r, "/ws", 8080);
+require('./router_websockets.js').init(r, "", 8080);
 require('./router_console_in.js').init(r, "");
 //require('./router_random_in.js').init(r, "/ethercat/Engel/Energie_P1", 20, 0, 100);
 require('./router_childprocess.js').init(r, "/plugwise", "/home/pi/plugwise_bridge/scripts/plugwise_log.pl", ["/dev/ttyUSB0"]);
