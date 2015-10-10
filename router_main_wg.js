0498ab03fc58fe15324184b1afe21a3b767cef36
Simon Walz <contact@simonwalz.de>
2015-10-10 21:01:05 +0200

    Router, main, WG

---
+++ router_main_wg.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+var Router = require('./router.js').router;
+var r = new Router();
+
+require('./router_console_out.js').init(r, "/console");
+require('./router_websockets.js').init(r, "", 8080);
+//require('./router_random_in.js').init(r, "/ethercat/Engel/Energie_P1", 20, 0, 100);
+
+require('./router_io_function.js').init(r);
+require('./router_io_mean.js').init(r);
+require('./router_io_bias.js').init(r);
+require('./router_io_multiply.js').init(r);
+require('./router_io_sum.js').init(r);
+
+require('./router_execcommand.js').init(r);
+
+require('./router_rcswitch.js').init(r, "/WG", {
+	"/Simon/Bettlampe":	"10011-4",
+	"/Simon/Stein":		"10011-3",
+	"/Simon/Strahler oben":	"10011-1",
+	"/Simon/Strahler unten":	"10011-2",
+	"/Simon/Schreibtischlampe":	"10100-1",
+	"/Simon/Schreibtisch Spot":	"10100-2",
+	"/Simon/Buntes Licht":	"10100-3",
+	"/Simon/Sirene":	"10100-4"
+});
+
