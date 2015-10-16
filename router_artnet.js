3107011255ec047b7a694237d74024fa11f858d9
Simon Walz <contact@simonwalz.de>
2015-10-16 23:17:27 +0200

    Router, artnet

---
+++ router_artnet.js
@@ -0,0 +1,25 @@
+/*
+ * TODO:
+ * modul artnet aendern:
+ *
+ * that.send(universe, 512, callback);
+ */
+
+exports.init = function(router, basename, options, nodes) {
+	var artnet = require('artnet')(options);
+
+	router.dests.artnet = function(id, time, value, name, obj) {
+		channel = id;
+		artnet.set(channel, value);
+
+		var dnode = name.replace(/_s$|@s$/, "");
+		router.route(dnode, time, value);
+	};
+
+	for (var n in nodes) {
+		var channel = nodes[n];
+
+		router.register(basename + n + '_s', 'artnet', channel);
+	}
+};
+
