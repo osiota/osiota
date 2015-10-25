ad56e5f4e3cef5df9079c27d4690233d2e3b9395
Simon Walz <contact@simonwalz.de>
2015-10-26 00:02:08 +0100

    Router, console in: format -time

--- router_console_in.js
+++ router_console_in.js
@@ -8,7 +8,7 @@ exports.init = function(router, basename) {
 			var lines = str.split(/\r?\n/g);
 			for (var i=0; i<lines.length; i++) {
 				if (lines[i] != "") {
-					var result = lines[i].match(/^([^\[]+)\s+\[([0-9.]+)\]:\s+([-0-9.]+|undefined)$/);
+					var result = lines[i].match(/^([^\[]+)(?:\s+\[([0-9.]+)\])?:\s+([-0-9.]+|undefined)$/);
 					if (result) {
 						var name = result[1];
 						var time = result[2];
