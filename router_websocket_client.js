e85fb8d5380be348aea2d371680e722d1941c871
Simon Walz <contact@simonwalz.de>
2015-11-12 18:51:08 +0000

    Router, Websocket client: +relative path

--- router_websocket_client.js
+++ router_websocket_client.js
@@ -77,8 +77,8 @@ exports.init = function(router, basename, ws_url, init_callback) {
 	o_ws.request = function(node) {
 		o_ws.respond({"type":"bind", "node":node});
 	};
-	router.dests.wsc = function(id, time, value, name, obj) {
-		o_ws.send_data(name, time, value);
+	router.dests.wsc = function(id, time, value, name, obj, relative_name) {
+		o_ws.send_data(id + relative_name, time, value);
 	};
 
 	return o_ws;
