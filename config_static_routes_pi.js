4881f1acdeee24088d7ab5b4a1850899df9ea7f8
Simon Walz <contact@simonwalz.de>
2015-11-12 13:54:32 +0100

    pi: +config_static_routes

--- config_static_routes_pi.js
+++ config_static_routes_pi.js
@@ -1,5 +1,10 @@
 exports.static_routes = {
-//'/ethercat/DistributionA/PLC':'/mysql/DistributionA/PLC',
-//'/ethercat/DistributionB/Airflow':'/mysql/DistributionB/Air',
+	'/plugwise/s_278EDF8/energy': '/Küche/Warmwasserboiler',
+	'/plugwise/s_28FBB58/energy': '/Küche/Wasserkocher',
+	'/plugwise/s_28FD198/energy': '/Küche/Geschirrspüler',
+	'/plugwise/s_28FD31F/energy': '/Küche/Kühlschrank',
+	'/plugwise/s_28FD32D/energy': '/Küche/Kaffeevollautomat',
+	'/plugwise/s_28FD4EE/energy': '/Küche/Kaffeemaschine',
+	'/plugwise/s_28FB858/energy': '/Küche/Microwelle'
 };
 
