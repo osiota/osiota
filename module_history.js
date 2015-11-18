b913b9f8f692c6ab6929fcb34a34af948c1df09e
Simon Walz <contact@simonwalz.de>
2015-11-18 16:29:44 +0100

    Module, History: +binary search

--- module_history.js
+++ module_history.js
@@ -2,7 +2,51 @@
 
 (function(exports){
 
-	var binarysearch = require("binary-search");
+	// from https://github.com/darkskyapp/binary-search
+	var binarysearch = function(haystack, needle, comparator, low, high) {
+		var mid, cmp;
+
+		if(low === undefined)
+			low = 0;
+
+		else {
+			low = low|0;
+			if(low < 0 || low >= haystack.length)
+				throw new RangeError("invalid lower bound");
+		}
+
+		if(high === undefined)
+			high = haystack.length - 1;
+
+		else {
+			high = high|0;
+			if(high < low || high >= haystack.length)
+				throw new RangeError("invalid upper bound");
+		}
+
+		while(low <= high) {
+			/* Note that "(low + high) >>> 1" may overflow, and results in a typecast
+			* to double (which gives the wrong results). */
+			mid = low + (high - low >> 1);
+			cmp = +comparator(haystack[mid], needle);
+
+			/* Too low. */
+			if(cmp < 0.0) 
+				low  = mid + 1;
+
+			/* Too high. */
+		else if(cmp > 0.0)
+			high = mid - 1;
+
+		/* Key found. */
+	else
+		return mid;
+		}
+
+		/* Key not found. */
+		return ~low;
+	};
+
 
 	/* history */
 	exports.history = function(history_length) {
@@ -52,5 +96,5 @@
 	}
 
 
-})(typeof exports === 'undefined'? this['history']={}: exports);
+})(typeof exports === 'undefined'? this['module_history']={}: exports);
 
