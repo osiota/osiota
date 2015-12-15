bbb039830e74b61e11577c94f4691f1a4e323cda
Simon Walz <contact@simonwalz.de>
2015-12-15 17:31:45 +0100

    Router, main: example

---
+++ router_main_example.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+var Router = require('./router.js').router;
+var r = new Router();
+
+require('./router_console_out.js').init(r, "/console");
+require('./router_websockets.js').init(r, "", 8080);
+
+require('./router_io_function.js').init(r);
+require('./router_io_mean.js').init(r);
+require('./router_io_bias.js').init(r);
+require('./router_io_multiply.js').init(r);
+require('./router_io_sum.js').init(r);
+
+require('./router_random_in.js').init(r, "/random", 20, 0, 100);
+
