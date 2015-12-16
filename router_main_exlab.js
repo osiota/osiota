bb1cae73d0cd900c56265adeb3e8622a687ad1e2
Simon Walz <contact@simonwalz.de>
2015-12-16 17:33:43 +0100

    Router, main, IWF: use local configs and relative paths

--- router_main_exlab.js
+++ router_main_exlab.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-var mysql_config = require("router_main_exlab_config.js").mysql_config;
+var mysql_config = require("./router_main_exlab_config.js").mysql_config;
 
 var Router = require('./router.js').router;
 var r = new Router();
--- router_main_relab.js
+++ router_main_relab.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-var mysql_config = require("router_main_relab_config.js").mysql_config;
+var mysql_config = require("./router_main_relab_config.js").mysql_config;
 
 var Router = require('./router.js').router;
 var r = new Router();
