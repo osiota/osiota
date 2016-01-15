46a1a4b2ac9dca0d8caf39341c1e4b1a7b60d564
Chao Suo <c.suo@tu-bs.de>
2016-01-15 18:16:39 +0100

    Router, client: +history command

--- router_main_client.js
+++ router_main_client.js
@@ -7,6 +7,9 @@ var optimist = require('optimist')
 	.default('server', "ws://localhost:8080/")
 	.alias('list', 'l')
 	.describe('list', 'List nodes of the router')
+	.alias('history', 'his')
+	.describe('history', 'Get history data with interval for all nodes\n\tinterval in seconds')
+	.default('history', null)
 	.alias('help', 'h')
 	.describe('help', 'Display the usage');
 var argv = optimist.argv;
@@ -30,11 +33,20 @@ require('./router_websocket_client.js')
 		o_ws.sendjson({"type":"list"});
 
 	if (argv._.length > 0) {
-		for(i=0; i<argv._.length; i++) {
-			var node = argv._[i].toString();
-			o_ws.request(node);
-			r.register(node, "console", node);
-		}
+        for(i=0; i<argv._.length; i++)
+        {
+            var node = argv._[i].toString();
+			if (argv.history !== null) {
+				o_ws.respond({
+					"type":"get_history",
+					"node":node,
+					"interval": argv.history
+				});
+			} else {
+				o_ws.request(node);
+				r.register(node, "console", node);
+			}
+        }
 	} else {
 		console.log("no node registered. existing ...");
 		setTimeout(function() { process.exit(); }, 1000);
