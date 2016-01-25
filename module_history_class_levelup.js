b59ee883abed55e90b68e8a304a895e9a51b8a94
Chao Suo <c.suo@tu-bs.de>
2016-01-25 16:03:14 +0100

    improved module_history_class_levelup.js, version = time

--- module_history_class_levelup.js
+++ module_history_class_levelup.js
@@ -11,10 +11,9 @@ exports.history = function (nodeName, history_config) {
 
 exports.history.prototype.add = function (time, value) {
 	var nodeName = this.nodeName;
-	var json = {"time":time, "value":value};
 
 	//save data without interval
-	this.db.put(nodeName, JSON.stringify(json), function (err, version) {
+	this.db.put(nodeName, value, {version: time}, function (err, version) {
 		if(err) return console.log('Error:', err);
 	});
 };
@@ -23,18 +22,11 @@ exports.history.prototype.add = function (time, value) {
 exports.history.prototype.get = function (interval, callback) {
 	var _this = this;
 	var nodeName = this.nodeName;
-	var lastTime;
-	var newTime;
-	var newValue;
-	var sum;
-	var count;
-	var globalCount;
 	var hdata = [];
-	var hdataTemp = [];
 
 	this.db.createVersionStream(nodeName)
 	.on('data', function (data) {
-		var json = JSON.parse(data.value);
+		var json = {"time":data.version, "value":data.value};
 		hdata.unshift(json);
 	})
 	.on('error', function (err) {
@@ -46,44 +38,42 @@ exports.history.prototype.get = function (interval, callback) {
 	.on('end', function() {
 		if(interval == 0)
 			callback(hdata);
-		else
-		{
-			if(typeof lastTime === "undefined")
-			{
-				lastTime = hdata[hdata.length - 2].time*1000;
-				sum = 0;
-				count = 0;
-			}
-			for(var i = hdata.length - 2; i >= 0; i--)
-			{
-				newTime = hdata[i].time*1000;
-				//to check, if time of history data is continuously
-				if(hdata[i+1].time*1000 - newTime < 1000)
-				{
-					if(Math.floor((lastTime - newTime) / (interval*1000)) == 1)
-					{
-						lastTime = newTime;
-						newValue = sum / count;
-						sum = 0;
-						count = 0;
-						var newJson = {"time":hdata[i].time, "value":newValue};
-						hdataTemp.unshift(newJson);
-					} 
-					else 
-					{
-						sum += hdata[i].value;
-						count ++;
-						globalCount ++;
-						if(globalCount >= _this.maxCount)
-							break;
-					}
-				} 
-				else
-					lastTime = newTime;
-			}
+		else {
+			var hdataTemp = _this.change_timebase(hdata, interval);
 			callback(hdataTemp);
 		}
 		console.log('Getting history stream with ' + interval + 's interval ended.');
 	});
+};
+
+exports.history.prototype.change_timebase = function (hdata, interval) {
+	var hdataTemp = [];
+
+	var lastTime = hdata[hdata.length - 2].time*1000;
+	var sum = 0;
+	var count = 0;
+
+	for (var i = hdata.length - 2; i >= 0; i--) {
+		var newTime = hdata[i].time*1000;
+		//to check, if time of history data is continuously
+		if (hdata[i+1].time*1000 - newTime < 1000) {
+			if (Math.floor((lastTime - newTime) / (interval*1000)) == 1) {
+				lastTime = newTime;
+				var newValue = sum / count;
+				sum = 0;
+				count = 0;
+				var newJson = {"time":hdata[i].time, "value":newValue};
+				hdataTemp.unshift(newJson);
+			} 
+			else {
+				sum += parseInt(hdata[i].value);
+				count ++;
+			}
+		} 
+		else
+			lastTime = newTime;
+	}
+
+	return hdataTemp;
 }
 
