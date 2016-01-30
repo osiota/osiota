af28c0b1c875c0fdd0d00ccfcf6f771523705ab5
Chao Suo <c.suo@tu-bs.de>
2016-01-30 19:22:58 +0100

    save data with callback function

--- module_history_class_levelup.js
+++ module_history_class_levelup.js
@@ -1,39 +1,117 @@
 var levelUP = require("levelUP");
 var version = require("level-version");
 
+/*version.prototype.add = function(nodeName, time, value) {
+	this.put(nodeName, value, {version: time}, function (err, version) {
+		if(err) return console.log('Error:', err);
+	});
+};*/
+
+var helper_change_timebase = function (interval, callback) {
+	var sum = 0;
+	var count = 0;
+	var lastTimeSlot = 0;
+
+	return function(nodeName, time, value) {
+		if (interval == 0) {
+			callback(nodeName, time, value, interval);
+		}
+		else {
+			var thisTimeSlot = Math.floor(time / interval) * interval;
+			if (thisTimeSlot != lastTimeSlot) {
+				if (count != 0) {
+					var newValue = sum / count;
+					var newTime = lastTimeSlot + interval;
+					callback(nodeName, newTime, newValue, interval);
+				}
+
+				sum = 0;
+				count = 0;
+
+				lastTimeSlot = thisTimeSlot;
+			}
+			sum += value;
+			count ++;
+		}
+	};
+}
+
 exports.history = function (nodeName, history_config) {
 	var _this = this;
 	this.nodeName = nodeName;
-	var dbName = nodeName.replace("/", "");
 	this.timebases = history_config.timebases;
-	this.original = {};
-	this.db = {};
-	this.timebases.forEach (function(d) {
-		_this.original[d.delta_t] = levelUP('./level_db/' + dbName + '/' + d.filename);
-		_this.db[d.delta_t] = version(_this.original[d.delta_t]);
-	});
+//	this.timebases = {};
+//	this.timebases[nodeName] = history_config.timebases;
+	levelUP('./level_db' + nodeName);
+/*	for (var t = 0; t < this.timebases.length; t++) {
+		var ldb = levelUP('./level_db' + nodeName + '/' + this.timebases[t].filename);
+		var vdb = version(ldb);
+		this.timebases[t].vdb = vdb;
+		
+		this.timebases[t].put = helper_change_timebase(this.timebases[t].delta_t, function(nodeName, time, value) {
+//			vdb.add(nodeName, time, value);
+			vdb.put(nodeName, value, {version: time}, function (err, version) {
+				if(err) return console.log('Error:', err);
+			});
+		});
+	};*/
+	for (var t = 0; t < this.timebases.length; t++) {
+		this.timebases[t].ldb = levelUP('./level_db' + nodeName + '/' + this.timebases[t].filename);
+		this.timebases[t].vdb = version(this.timebases[t].ldb);
+		
+		this.timebases[t].put = helper_change_timebase(this.timebases[t].delta_t, function(nodeName, time, value, interval) {
+			if (interval == 0) {
+				_this.timebases[0].vdb.put(nodeName, value, {version: time}, function (err, version) {
+					if(err) return console.log('Error:', err);
+				});
+			}
+			else if (interval == 1) {
+				_this.timebases[1].vdb.put(nodeName, value, {version: time}, function (err, version) {
+					if(err) return console.log('Error:', err);
+					console.log(nodeName)
+				});
+			}
+			else if (interval == 60) {
+				_this.timebases[2].vdb.put(nodeName, value, {version: time}, function (err, version) {
+					if(err) return console.log('Error:', err);
+				});
+			}
+			else if (interval == 60*60) {
+				_this.timebases[3].vdb.put(nodeName, value, {version: time}, function (err, version) {
+					if(err) return console.log('Error:', err);
+				});
+			}
+			else {
+				console.log("Error(save data to database): System doesn't have database with " + interval + " interval.");
+			}
+		});
+	};
 	this.maxCount = history_config.maxCount;
 };
 
 exports.history.prototype.add = function (time, value) {
 	var nodeName = this.nodeName;
-
-	//save data without interval
-	this.db[0].put(nodeName, value, {version: time}, function (err, version) {
-		if(err) return console.log('Error:', err);
+	this.timebases.forEach(function(d) {
+		d.put(nodeName, time, value);
 	});
 };
 
 //remote getting history
 exports.history.prototype.get = function (interval, callback) {
-	var _this = this;
 	var nodeName = this.nodeName;
 	var hdata = [];
 
-	this.db[0].createVersionStream(nodeName)
+	var vdb;
+	this.timebases.forEach(function(d) {
+		if (d.delta_t == interval)
+			vdb = d.vdb;
+	});
+	if (typeof vdb === "undefined")
+		return;
+	vdb.createVersionStream(nodeName)
 	.on('data', function (data) {
 		var json = {"time":data.version, "value":data.value};
-		hdata.unshift(json);
+		hdata.push(json);
 	})
 	.on('error', function (err) {
 		console.log('Error from getting history:',err);
@@ -42,61 +120,22 @@ exports.history.prototype.get = function (interval, callback) {
 		console.log('Getting history stream closed.');
 	})
 	.on('end', function() {
-		if(interval == 0)
-			callback(hdata);
-		else {
-			var hdataTemp = _this.change_timebase(hdata, interval);
-			callback(hdataTemp);
-		}
+		callback(hdata);
 		console.log('Getting history stream with ' + interval + 's interval ended.');
 	});
 };
 
-exports.history.prototype.change_timebase = function (hdata, interval) {
+/*exports.history.prototype.change_timebase = function (hdata, interval, callback) {
 	var hdataTemp = [];
-
-	var lastTime = Math.floor(hdata[0].time / interval);
-	var sum = 0;
-	var count = 0;
-
-	if (interval > 0) {
-		for (var i = 0; i < hdata.length; i++) {
-			//first value
-			if (i == 0) {
-				sum += parseInt(hdata[i].value);
-				count ++;
-			}
-			//time is continuous
-			else if (hdata[i].time - hdata[i-1].time < 1) {
-				if (Math.floor(hdata[i].time / interval) == lastTime) {
-					sum += parseInt(hdata[i].value);
-					count ++;
-				} 
-				else {
-					sum += parseInt(hdata[i].value);
-					count ++;
-					var newValue = sum / count;
-					sum = 0;
-					count = 0;
-					var newJson = {"time":hdata[i].time, "value":newValue};
-					hdataTemp.unshift(newJson);
-					lastTime = Math.floor(hdata[i].time / interval)
-				}
-			//time is not continuous
-			} else {
-				sum += parseInt(hdata[i].value);
-				count ++;
-				var newValue = sum / count;
-				sum = 0;
-				count = 0;
-				var time = lastTime + interval;
-				var newJson = {"time":time, "value":newValue};
-				hdataTemp.unshift(newJson);
-				lastTime = Math.floor(hdata[i].time / interval)
-			} 
-		}
-
-		return hdataTemp;
-	}
-}
+	var hct = helper_change_timebase(interval, function() {
+		hdataTemp.push({
+			"time": time,
+			"value": value
+		});
+	});
+	hdata.forEach(function(d) {
+		hct(d.time, d.value);
+	});
+	return hdataTemp;
+};*/
 
