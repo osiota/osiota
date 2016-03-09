b5a28361534cf8e6ec9eeaaa8df3d724d0b0856d
Simon Walz <contact@simonwalz.de>
2016-03-09 14:47:26 +0000

    Router, main, pi53: +PC Richter

--- config_static_routes_pi53.js
+++ config_static_routes_pi53.js
@@ -1,7 +1,7 @@
 exports.static_routes = {
 	'/plugwise/s_29C283E/energy': '/Raum_215/PC_Walz',
 	'/plugwise/s_355852E/energy': '/Raum_215/PC_Rother',
-	'/plugwise/s_28FB7CB/energy': '/Raum_215/PC Richter',
+	'/plugwise/s_28FB7CB/energy': '/Raum_215/PC_Richter',
 	'/Raum_215': '/Büro'
 };
 
