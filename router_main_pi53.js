5312bbadb321557620e6bc81d9386d3073aa8c86
Simon Walz <contact@simonwalz.de>
2016-03-09 12:17:13 +0000

    Router, main, pi: +childprocess command line for plugwise changed

--- router_main_pi53.js
+++ router_main_pi53.js
@@ -11,7 +11,7 @@ require('./router_console_out.js').init(r, "/console");
 require('./router_websockets.js').init(r, "", 8080);
 require('./router_console_in.js').init(r, "");
 //require('./router_random_in.js').init(r, "/ethercat/Engel/Energie_P1", 20, 0, 100);
-require('./router_childprocess.js').init(r, "/plugwise", "/home/pi/plugwise_bridge/scripts/plugwise_log.pl", ["/dev/ttyUSB0"]);
+require('./router_childprocess.js').init(r, "/plugwise", "../energy-router-plugwise/plugwise_log.pl", ["/dev/ttyUSB0"]);
 
 require('./router_io_function.js').init(r);
 require('./router_io_mean.js').init(r);
