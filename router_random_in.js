e9cabba63a9a71754b9cea6836a91bd97c24bca1
Simon Walz <contact@simonwalz.de>
2016-06-24 12:14:28 +0200

    module: random, more options

--- router_random_in.js
+++ router_random_in.js
@@ -1,18 +1,27 @@
 
-exports.init = function(router, basename, delay, cmin, cmax) {
+exports.init = function(router, basename, delay, cmin, cmax, exp, round) {
 	if (typeof delay === "undefined")
 		delay = 1000;
 	if (typeof cmin === "undefined")
 		cmin = 0;
 	if (typeof cmax === "undefined")
 		cmax = 100;
+	if (typeof exp !== "number")
+		exp = 5;
+	if (typeof round !== "number")
+		round = null;
 
-	var last_value = (cmax - cmin);
+	var last_value = 0.5;
 	setInterval(function(router, basename, cmin, cmax) {
-		var v = Math.random() * (cmax - cmin) + cmin;
-		v = last_value + (v-(cmax-cmin)/2)/5;
-		v = Math.max(Math.min(v, cmax), cmin);
+		var v = Math.random();
+		v = last_value + (v-0.5)/exp;
+		v = Math.max(Math.min(v, 1), 0);
 		last_value = v;
+
+		v = v * (cmax - cmin) + cmin;
+		if (round !== null) {
+			v = Math.round( v * Math.pow(10, round) ) / Math.pow(10, round);
+		}
 		router.node(basename).publish(undefined, v);
 	}, delay, router, basename, cmin, cmax);
 
