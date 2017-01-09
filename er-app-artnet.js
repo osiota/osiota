e82cfa695827ea267973ca893b91de920cee1b60
Simon Walz <contact@simonwalz.de>
2017-01-09 13:31:29 +0100

    er-app-usbdmx: rewritten (at the weekend)

--- er-app-artnet.js
+++ er-app-artnet.js
@@ -12,16 +12,34 @@ exports.init = function(node, app_config, main, host_info) {
 	var artnet = require('artnet')(options);
 
 	for (var n in nodes) {
-		let channel = nodes[n];
+		var channel = nodes[n];
+		var default_value = null;
+		var nn = node.node(n);
 
-		node(n).rpc_set(function(value) {
+		if (Array.isArray(channel) && channel.length >= 2) {
+			default_value = channel[1];
+			channel = channel[0];
+		}
+
+		(function(nn, channel) {
+		nn.rpc_set = function(reply, value) {
 			if (typeof value !== "number")
 				value *= 1;
 			if (value !== null)
 				artnet.set(channel, value);
 
 			this.publish(undefined, value);
-		});
+
+			reply(null, "ok");
+		};
+		})(nn, channel);
+
+		if (nn.value !== null) {
+			default_value = nn.value;
+		}
+		if (default_value !== null) {
+			nn.rpc_set(function() {}, default_value);
+		}
 	}
 };
 
