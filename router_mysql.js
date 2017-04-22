f8aa915d9b0d6e999f0391d48d74866967b26ada
Simon Walz <contact@simonwalz.de>
2017-04-22 14:08:04 +0200

    router, mysql: using cue getter

--- router_mysql.js
+++ router_mysql.js
@@ -17,23 +17,31 @@ exports.init = function(router, basename, mysql_config) {
 
 	// a simple query function using Mysql Pool Connections
 	exports.query = function(query, data, callback) {
-		if (typeof data === "undefined") data = [];
 		exports.pool.getConnection(function(err, connection) {
 			if (err) {
 				if (connection)
 					connection.release();
 				console.warn("mysql: Error while asking for a connection:", err);
+				if (typeof data === "function") {
+					data(err);
+				}
+
 				return;
 			}
 			if (!connection.listeners("error")) {
 				connection.on("error", function(err) {
 					console.warn("mysql: Error while processing query. Error:", err);
+					connection.release();
 				});
 			}
+			if (typeof data === "function") {
+				data = data(false);
+			}
+			if (typeof data === "undefined") data = [];
 			connection.query(query, data, function(err, rows, fields) {
 				connection.release();
 				if (err) {
-					console.log('mysql: Error while performing query:', err);
+					console.warn('mysql: Error while performing query:', err);
 					return;
 				}
 				if (typeof callback === "function")
@@ -41,30 +49,31 @@ exports.init = function(router, basename, mysql_config) {
 			});
 		});
 	};
-	exports.insertdata = router.cue(function(data) {
+	exports.insertdata = router.cue_getter(function(getter) {
 		var table = "Data";
 		var keys = ["Measurement_id", "Time", "Value"];
-		//var sqlq = mysql.format("INSERT INTO ??(??) VALUES ?", [table, keys, data]);
-		//console.log("Query: ", sqlq);
-		exports.query("INSERT INTO ??(??) VALUES ?", [table, keys, data]);
-	});
-	// Register MySQL Destination:
-	var module_name = router.register_static_dest("mysql", function(node, relative_name, do_not_add_to_history) {
-		// this = rentry
-		if (typeof node.value !== "undefined" && node.value !== null)
-			exports.insertdata([this.id, node.time, node.value]);
-			//exports.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + id + ', ' + time + ', ' + value + ')');
-	});
 
+		//var sqlq = mysql.format("INSERT INTO ??(??) VALUES ?", [table, keys, data]);
+		exports.query("INSERT INTO ??(??) VALUES ?", function(err) {
+			data = getter(err);
+			if (err) return;
+			return [table, keys, data];
+		});
+	});
 
 	// Get measurement names
-	var mid = {};
 	exports.query('SELECT m.id, CONCAT(s.Name, "/", m.Name) AS node FROM Measurement AS m LEFT JOIN Station AS s ON m.Station_id = s.id;', [], function(rows, fields) {
-		for(var i=0;i<rows.length;i++) {
-			mid[rows[i].node] = rows[i].id;
+		rows.forEach(function(row) {
+			var entry_id = row.id;
+			router.node(basename + "/" + row.node).subscribe(function() {
+				// this = node
+				var node = this;
 
-			var rentry = router.node(basename + "/" + rows[i].node).register(module_name, rows[i].id);
-		}
+				if (typeof node.value !== "undefined" && node.value !== null) {
+					exports.insertdata([entry_id, node.time, node.value]);
+					//exports.query('INSERT INTO Data(Measurement_id, Time, Value) VALUES(' + id + ', ' + time + ', ' + value + ')');
+				}
+			});
+		});
 	});
-
 };
