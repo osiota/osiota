2b0a54e265431e440935ab06aaf0d03877d08d65
Simon Walz <contact@simonwalz.de>
2017-05-22 18:36:15 +0200

    init: systemd startup install script

---
+++ install_systemd_service.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ER_PATH=`pwd`
+ER_CONFIG="config"
+ER_NAME="energy-rotuer"
+ER_USER="${USER}"
+
+#if test \! -f "main"
+#then
+#	echo "Run this script in the energy-router repo"
+#	exit 2
+#fi
+
+cat <<EOF | sudo "cat >/etc/systemd/system/${ER_NAME}.service"
+[Unit]
+Description=${ER_NAME}
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=${ER_PATH}/main "${ER_CONFIG}"
+Restart=on-failure
+User=${ER_USER}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable "${ER_NAME}"
+#systemctl start "${ER_NAME}"
+
