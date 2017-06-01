4cc2a4c5de895f7a0c36d4ac49e6b43553eb3201
Simon Walz <contact@simonwalz.de>
2017-06-01 23:29:16 +0200

    er-app-device: altered variable name

--- er-app-device.js
+++ er-app-device.js
@@ -1,10 +1,10 @@
 
-exports.init = function(app_node, app_config, main, host_info) {
+exports.init = function(node, app_config, main, host_info) {
 
 	if (typeof app_config.metadata !== "object") {
 		app_config.metadata = {};
 	}
 	app_config.metadata.type = "device.object";
 
-	app_node.announce(app_config.metadata);
+	node.announce(app_config.metadata);
 };
