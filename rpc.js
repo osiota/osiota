afc5069dca25862a713335cfbf0a9e0cdcb1f37a
Simon Walz <contact@simonwalz.de>
2017-06-12 16:13:45 +0200

    rpc.js: new cli script to run rpc commands from apps

---
+++ rpc.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var main = require("./main.js");
+var m = new main();
+
+var argv = require('yargs')
+	.usage('Usage: $0 [-app_config]', {
+	})
+	.example("$0 --install_dir=./ install-apps-git install_app rest-api")
+	.help().version()
+	.argv;
+
+var app = argv._.shift();
+
+m.startup(null, app, argv, undefined, undefined,
+		function(a) {
+	argv._.push(function(err, result) {
+		if (err) {
+			throw err;
+		}
+		console.log("result", result);
+	});
+	a._node.rpc.apply(a._node, argv._);
+});
