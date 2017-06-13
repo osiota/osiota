101fd930a43c5bbb3f6e6b3a40701cc6246bc3d4
Simon Walz <contact@simonwalz.de>
2017-06-13 18:29:39 +0200

    admin: test +dirs

--- er-app-admin-test.js
+++ er-app-admin-test.js
@@ -1,5 +1,7 @@
 
 var load_schema_apps = require("./er-app-admin.js").load_schema_apps;
 
-load_schema_apps(["./", "../", __dirname+"/", __dirname+"/../",
+var s = load_schema_apps(["./", "../", __dirname+"/", __dirname+"/../",
 		__dirname+"/node_modules/"]);
+
+console.log("schema", JSON.stringify(s, undefined, "  "));
