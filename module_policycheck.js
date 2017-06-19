41d905a45a8a30bc1754e338646e31c99834d276
Simon Walz <contact@simonwalz.de>
2017-06-19 15:32:20 +0200

    policycheck: reformatted

--- module_policycheck.js
+++ module_policycheck.js
@@ -23,8 +23,8 @@ var v = require('./module_json_validator');
  * assigns a policy-action to a policy-type
  */
 var TYPE_TO_ACTION = {
-    read: ['hide_all', 'hide_value_and_metadata', 'hide_value', 'preprocess_value', 'forward_all'],
-    write: ['block_write', 'allow_write']
+	read: ['hide_all', 'hide_value_and_metadata', 'hide_value', 'preprocess_value', 'forward_all'],
+	write: ['block_write', 'allow_write']
 };
 
 /*
@@ -41,14 +41,14 @@ var TYPE_TO_ACTION = {
  * to be evaluated in the szenario and won't do a full Policy-Check!
  */
 var TYPE_TO_METHOD = {
-    to_remote: {
-        read: ['announce'],
-        write: []
-    },
-    from_remote:{
-        read: ['subscribe'],
-        write: ['data', 'announce']
-    }
+	to_remote: {
+		read: ['announce'],
+		write: []
+	},
+	from_remote:{
+		read: ['subscribe'],
+		write: ['data', 'announce']
+	}
 };
 
 
@@ -116,6 +116,7 @@ function get_reaction(node, remote_id, method, data_flow, policy) {
 	if (typeof REACTIONS[data_flow] === "object" &&
 			typeof REACTIONS[data_flow][method] === "object" &&
 			Array.isArray(REACTIONS[data_flow][method]) &&
+			typeof policy_action === "string" &&
 			REACTIONS[data_flow][method]
 				.indexOf(policy_action) > -1) {
 
@@ -184,55 +185,47 @@ exports.Policy_checker.prototype.add_policy = function(policy_level, policy) {
 				'default_level, application_level, user_level'
 			);
 		}
-		this.activate_policy(policy_level, policy);
 	} else {
 		console.log("Your policy "+ policy.toString() +" is not valid");
 	}
 };
 
-exports.Policy_checker.prototype.activate_policy = function(policy_level,
-								policy) {
-	/*
-	if (policy.action == 'preprocess_value') {
-		// aggregating data of group of nodes
-		if (policy.action_extra.hasOwnProperty('group')) {
-			this.aggregation.activate_policy(policy);
+
+/* 
+ * Find the relevant policy dependent on the node, remote, metadata
+ * and policy_type
+ *    - node: node-object
+ *    - remote: remote-id like "ws://localhost:8080"
+ *    - metadata: metadata-object
+ *    - policy-type: can be "read" or "write"
+ */
+exports.Policy_checker.prototype.find_most_relevant_policy = function (node,
+		remote, metadata, policy_type) {
+	var mostRelevantPolicy;
+	var mostRelevantMatchscore = [[], [], []];
+	for (var x = 2; x >= 0; x--) {
+		var policies = this.policy_set[x];
+		for (var y = 0; y < policies.length; y++) {
+			var policy = policies[y];
+			if (TYPE_TO_ACTION[policy_type].indexOf(
+						policy.action) > -1) {
+				var match = check_if_relevant(policy,
+						node, remote, metadata);
+				if (match[0] == true && check_if_more_relevant(
+						match[1],
+						mostRelevantMatchscore
+				)) {
+					mostRelevantPolicy = policy;
+					mostRelevantMatchscore = match[1];
+				}
+			}
 		}
 	}
-	*/
-};
-
-/*returns the relevant policy from the given policies-array dependent on the node, remote, metadata and policy_type
-     - node: node-object
-     - remote: remote-id like "ws://localhost:8080"
-     - metadata: metadata-object
-     - policy-type: can be "read" or "write"
- */
-exports.Policy_checker.prototype.find_most_relevant_policy = function (node, remote, metadata, policy_type) {
-    var match;
-    var policy;
-    var policies;
-    var mostRelevantPolicy;
-    var mostRelevantMatchscore = [[], [], []];
-    for (var x = 2; x >= 0; x--) {
-        policies = this.policy_set[x];
-        for (var y = 0; y < policies.length; y++) {
-            if (TYPE_TO_ACTION[policy_type].indexOf(policies[y].action) > -1) {
-                policy = policies[y];
-                match = check_if_relevant(policies[y], node, remote, metadata);
-                if (match[0] == true) {
-                    if (check_if_more_relevant(match[1], mostRelevantMatchscore)) {
-                        mostRelevantPolicy = policies[y];
-                        mostRelevantMatchscore = match[1];
-                    }
-                }
-            }
-        }
-    }
-    if (typeof mostRelevantPolicy != 'undefined') {
-        return mostRelevantPolicy;
-    }
-    return 'default';//if no matching policy was found
+	if (typeof mostRelevantPolicy != 'undefined') {
+		return mostRelevantPolicy;
+	}
+	//if no matching policy was found
+	return 'default';
 };
 
 
@@ -241,120 +234,132 @@ exports.Policy_checker.prototype.find_most_relevant_policy = function (node, rem
  * read or write action
  */
 function get_type_by_method(method, data_flow) {
-    for (var type in TYPE_TO_METHOD[data_flow]) {
-        if (TYPE_TO_METHOD[data_flow][type].indexOf(method) > -1) {
-            return type;
-        }
-    }
-    return null;
-}
-
-function get_type_by_action(policy_action) {
-    for (var type in TYPE_TO_ACTION) {
-        if (TYPE_TO_ACTION[type].indexOf(policy_action) > -1) {
-            return type;
-        }
-    }
-    return null;
-}
-
-
-// -----------------------------Policy-related-helper-methods-----------------------------------------------
-
-function is_valid_policy(policy) {
-    return v.validate(policy, 'policy_schema');
+	for (var type in TYPE_TO_METHOD[data_flow]) {
+		if (TYPE_TO_METHOD[data_flow][type].indexOf(method) > -1) {
+			return type;
+		}
+	}
+	return null;
 }
 
 /*
- checks if the policy is relevant for the respective node-, remote- and metadata-combination
- and returns a matchScores-Array if it is a relevant policy. A policy is relevant when the defined values match
- following criteria:
-     - the node-variable either has to match the policy-node or be a child node of the policy-node
-     - the remote-variable has to match the policy-remote
-     - the metadata defined in the metadata-object has to match the policies-metadata
+ * Helper to get type from an policy action
+ * TODO: still unused!
+ */
+function get_type_by_action(policy_action) {
+	for (var type in TYPE_TO_ACTION) {
+		if (TYPE_TO_ACTION[type].indexOf(policy_action) > -1) {
+			return type;
+		}
+	}
+	return null;
+}
 
- matchScores[0]: =0 equals 'perfect match', =1 equals 'directParent', >1 equals indirectParent, nothing equals not defined
- matchScores[1]: =0 equals 'perfect match, empty equals not defined
- matchScores[2]: =0 all key/value-pairs match, >0 some key/value-pairs match, empty equals not defined
+
+// Policy-related-helper-methods
+
+/* 
+ * Check if policy is valid
+ */
+function is_valid_policy(policy) {
+	return v.validate(policy, 'policy_schema');
+}
+
+/*
+ * Check if a policy is relevant for a node
+ *
+ * Checks if the policy is relevant for the respective node-, remote-
+ * and metadata-combination and returns a matchScores array.
+ *
+ * A policy is relevant when the defined values match following criteria:
+ *    - the node variable either has to match the policy-node or be a child
+ *      node of the policy-node
+ *    - the remote variable has to match the policy-remote
+ *    - the metadata defined in the metadata-object has to match the
+ *      policies-metadata
+ *
+ * matchScores[0]:
+ * 	=0 equals 'perfect match',
+ * 	=1 equals 'directParent',
+ * 	>1 equals indirectParent, nothing equals not defined
+ *
+ * matchScores[1]:
+ *  	=0 equals 'perfect match,
+ *  	empty equals not defined
+ *
+ * matchScores[2]:
+ * 	=0 all key/value-pairs match,
+ * 	>0 some key/value-pairs match,
+ * 	empty equals not defined
  */
 function check_if_relevant(policy, node, remote, metadata) {
-    var node_name = node.name;
-    var relevant = true;
-    var matchScores = [[], [], []];
-    if (policy.hasOwnProperty('node')) {
-        if (policy.node == node_name || is_parentnode(policy.node, node)) {
-            matchScores[0].push(get_parent_level(node.router.node(policy.node),
-				    node));
-        } else {
-            relevant = false;
-        }
-    }
-    if (policy.hasOwnProperty('remote')) {
-        if (policy.remote == remote) {
-            matchScores[1].push(0);
-        } else {
-            relevant = false;
-        }
-    }
-    if (policy.hasOwnProperty('metadata')) {
-        if (match(metadata, policy.metadata)) {
-            matchScores[2].push(0 - ((Object.keys(metadata).length - Object.keys(policy.metadata).length)));
-        } else {
-            relevant = false;
-        }
-    }
-    return [relevant, matchScores];
-}
-
-/*
-checks if a relevant policie fits better then another based on the Match-Score.
-*/
-function check_if_more_relevant(policy_score_array1, policy_score_array2) {
-    for (var a = 0; a <= 2; a++) {
-        if (policy_score_array1[a].length == 1 && policy_score_array2[a].length == 0)
-            return true;
-        if (policy_score_array1[a].length == 0 && policy_score_array2[a].length == 1)
-            return false;
-        if (policy_score_array1[a] < policy_score_array2[a])
-            return true;
-        if (policy_score_array1[a] > policy_score_array2[a])
-            return false;
-    }
-    return false;
-}
-
-/*
-checks if the 'parent' is  parent of 'node'
-EXAMPLE for parent: ('/IBR/Miklab' , /IBR/Miklab/Lamp.energy)
-EXAMPLE for parent: ('/IBR , /IBR/Miklab/Lamp.energy)
-*/
-function is_parentnode(parent, node) {
-    if(parent == node.name){
-        return true;
-    }
-    if (node.parentnode !== null) {
-        return is_parentnode(parent, node.parentnode)
-    }
-    return false;
-}
-
-/*
- gets parentlevel for node
- EXAMPLE for parentlevel: ('/IBR/Miklab/Lamp.energy' , '/IBR/Miklab/Lamp.energy') -> level 0
- EXAMPLE for parentlevel: ('/IBR/Miklab' , '/IBR/Miklab/Lamp.energy') -> level 1
- EXAMPLE for parentlevel: ('/IBR' , '/IBR/Miklab/Lamp.energy') -> level 2
- EXAMPLE for parentlevel: ('/' , '/IBR/Miklab/Lamp.energy') -> level 3
-*/
-function get_parent_level(parent, child) {
-    var level = 0;
-    var node = child;
-    while(parent.name != node.name){
-        level ++;
-        node = node.parentnode
-	if (node === null) {
-		throw new Error("get_parent_level: child is not child of parent");
+	var node_name = node.name;
+	var relevant = true;
+	var matchScores = [[], [], []];
+	if (policy.hasOwnProperty('node')) {
+		if (policy.node == node_name) {
+			var l = is_parentnode(policy.node, node);
+			if (l >= 0) {
+				matchScores[0].push(l, node);
+			} else {
+				relevant = false;
+			}
+		} else {
+			relevant = false;
+		}
 	}
-    }
-    return level;
+	if (policy.hasOwnProperty('remote')) {
+		if (policy.remote == remote) {
+			matchScores[1].push(0);
+		} else {
+			relevant = false;
+		}
+	}
+	if (policy.hasOwnProperty('metadata')) {
+		if (match(metadata, policy.metadata)) {
+			matchScores[2].push(
+				Object.keys(policy.metadata).length
+				- Object.keys(metadata).length
+			);
+		} else {
+			relevant = false;
+		}
+	}
+	return [relevant, matchScores];
 }
 
+/*
+ * Checks if a relevant policy fits better then another based on the match score
+ */
+function check_if_more_relevant(policy_score_array1, policy_score_array2) {
+	for (var a = 0; a <= 2; a++) {
+		if (policy_score_array1[a].length == 1 &&
+				policy_score_array2[a].length == 0)
+			return true;
+		if (policy_score_array1[a].length == 0 &&
+				policy_score_array2[a].length == 1)
+			return false;
+		if (policy_score_array1[a] < policy_score_array2[a])
+			return true;
+		if (policy_score_array1[a] > policy_score_array2[a])
+			return false;
+	}
+	return false;
+}
+
+/*
+ * Checks if the 'parent' is a parent of 'node'
+ * 
+ * EXAMPLE for parent: ('/IBR/Miklab' , /IBR/Miklab/Lamp.energy)
+ * EXAMPLE for parent: ('/IBR , /IBR/Miklab/Lamp.energy)
+ */
+function is_parentnode(parent, node) {
+	if(parent == node.name)
+		return 0;
+	if (node.parentnode !== null) {
+		var r = is_parentnode(parent, node.parentnode);
+		if (r >= 0)
+			return r+1;
+	}
+	return -1;
+}
