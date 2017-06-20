061ca3f3a7322b5eba3bb0b5341b56f80872984d
Simon Walz <contact@simonwalz.de>
2017-06-20 19:26:10 +0200

    to-csv-file: updated app

---
+++ er-app-to-csv-file.js
@@ -0,0 +1,37 @@
+/* Save data in a csv file */
+
+// use graceful-fs to protect against EMFILE errors when opening to many files.
+//var fs = require('fs');
+var fs = require('graceful-fs');
+
+var module_appendFile = function(filename, content) {
+	fs.appendFile(filename, content, function(err) {
+		if (err) {
+			throw err;
+		}
+		// data appended successfully
+	});
+};
+
+exports.init = function(node, app_config, main, host_info) {
+
+	var prefix = "./data/";
+	if (typeof app_config.prefix === "string") {
+		prefix = app_config.prefix;
+	}
+	var filename = this._source.name.replace(/\//g, "_");
+	if (typeof app_config.filename === "string") {
+		filename = app_config.filename;
+	}
+
+	filename = prefix + filename + ".csv";
+
+	return this._source.subscribe(function() {
+		if (this.time === null) return;
+
+		if (this.value === null) return; //value = "null";
+
+		var content = this.time + "\t" + this.value + "\n";
+		module_appendFile(filename, content);
+	};
+};
--- router_console_out.js
+++
@@ -1,8 +0,0 @@
-
-exports.init = function(router, basename) {
-	router.dests.console = function(node, relative_name) {
-		console.log(this.id + relative_name + " [" + node.time + "]:\t" + node.value);
-	};
-	router.node(basename).register('console', basename);
-}
-
