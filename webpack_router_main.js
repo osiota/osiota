69feebbf40c3775bbe67a9a8d3795f877e1034d2
Simon Walz <contact@simonwalz.de>
2017-06-23 22:58:54 +0200

    webpack: changed localStorage module filename

--- webpack_router_main.js
+++ webpack_router_main.js
@@ -23,7 +23,7 @@ require('./module_history.js').init(r, history_config);
 //require('./router_random_in.js').init(r, "/random", 20, 0, 100);
 //require('./router_test.js').init(r, "/test", 100);
 
-r.localStorage = require('./router_localStorage.js').init.bind(r, r);
+r.localStorage = require('./er-app-local-storage.js').init;
 r.websocket_client = require('./router_websocket_client.js').init.bind(r, r);
 
 module.exports = r;
