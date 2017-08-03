6fa22960f0cc066ccf82f1fd360bf9c8f3aa0750
Simon Walz <contact@simonwalz.de>
2017-08-03 14:43:54 +0200

    app: json, play and save

---
+++ er-app-json-play.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+function consume_data(data, offset, cb, done, cleaning_object) {
+	var slots = 10000;
+	var i = offset;
+	var n = Math.min(offset+slots, data.length);
+	for (; i < n; i++) {
+		var d = data[i];
+		cb(d);
+	}
+	if (i >= data.length) {
+		if (typeof done === "function")
+			process.nextTick(done);
+	} else {
+		console.log("time", data[i].time);
+		var tid = setTimeout(consume_data, 0, data, n, cb, done,
+				cleaning_object);
+		cleaning_object[0] = tid;
+	}
+};
+
+exports.init = function(node, app_config, main, host_info) {
+	if (typeof app_config.filename !== "string") {
+		throw new Error("config option json filename not defined.");
+	}
+
+	node.announce({
+		"type": "unknown.data"
+	});
+
+	var cleaning_object = [];
+	
+	fs.readFile(app_config.filename, function(err, content) {
+		if (err) throw err;
+
+		var data = JSON.parse(content);
+
+		consume_data(data, 0, function(d) {
+			node.publish(d.time, d.value);
+			//node.publish_sync(d.time, d.value);
+		}, function() {
+			console.log("done");
+		}, cleaning_object);
+	});
+
+	return [cleaning_object, node];
+}
---
+++ er-app-json-save.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+exports.init = function(node, app_config, main, host_info) {
+	if (typeof app_config.filename !== "string") {
+		throw new Error("config option json filename not defined.");
+	}
+
+	var config_save_last = false;
+	if (typeof app_config.save_last !== "undefined") {
+		config_save_last = app_config.save_last;
+	}
+
+	var data = [];
+	var last_data = null;
+
+	var s = this._source.subscribe(function(do_not_add_to_history, initial){
+		if (initial)
+			return;
+		if (do_not_add_to_history) {
+			if (this.time !== null) {
+				last_data = {
+					"time": this.time,
+					"value": this.value
+				};
+				//console.log("ADD LAST DATA", last_data);
+			}
+			return;
+		}
+		last_data = null;
+
+		data.push({
+			"time": this.time,
+			"value": this.value
+		});
+
+	});
+
+	return [function(unload) {
+		setTimeout(function() {
+		console.log("Write json file");
+
+		if (config_save_last && last_data !== null) {
+			data.push(last_data);
+		}
+		fs.writeFile(app_config.filename,
+				JSON.stringify(data, null, "\t"),
+				function(err) {
+			if (err) throw err;	
+			console.log("json file written.");
+
+			unload(s);
+		});
+		data = [];
+
+		}, 100);
+	}];
+};
