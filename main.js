63ced2f3dfcaf6bb346bd9b2481d412743d072d1
Simon Walz <contact@simonwalz.de>
2017-09-21 13:48:59 +0200

    main: +property +subkey

--- main.js
+++ main.js
@@ -82,6 +82,109 @@ function main(router_name) {
 			node: callback
 		};
 	};
+	Node.prototype.property = function(name, type, callback, default_value){
+		if (typeof default_value === "undefined")
+			default_value = null;
+
+		var e = this.node(name);
+		e.rpc_set = function(reply, value) {
+			if (value === null) {
+				value = default_value;
+			} else {
+				if (typeof type === "string") {
+					if (typeof value !== type) {
+						throw new Error("Wrong data "+
+								"type");
+					}
+				} else {
+					// TODO: Check schema
+				}
+			}
+			this.publish(undefined, value);
+			if (typeof callback === "function") {
+				callback(value);
+			}
+			reply(null, "okay");
+		};
+		e.rpc_publish = function(reply, time, value) {
+			return this.rpc_set(reply, value);
+		};
+
+		// set default value:
+		e.rpc("set", null);
+
+		var ltype = type;
+		var schema;
+		if (typeof ltype !== "string") {
+			ltype = "schema";
+			schema = type;
+		} else {
+			schema = {
+				"type": type
+			};
+		}
+		e.announce({
+			"type": ltype+".property",
+			"property": true,
+			"schema": schema,
+			"rpc": {
+				"set": {
+					"desc": "Set",
+					"args": [true]
+				}
+			}
+		});
+
+		return e;
+	};
+	// todo: use subkey
+	Node.prototype.subnode = function(property, type) {
+		var _this = this;
+		var n = this.node(property);
+
+		var value = null;
+		var s = this.subscribe(function() {
+			var v = null;
+			value = this.value;
+			if (typeof value === "object" &&
+					value !== null) {
+				v = value[property];
+			}
+			n.publish(this.time, v);
+		});
+		n.rpc_set = function(reply, v) {
+			if (type && typeof v !== type) {
+				throw new Error("Type does not match target.");
+			}
+			if (typeof value !== "object" || value === null) {
+				value = {};
+			}
+			value[property] = v;
+
+			_this.rpc("set", value);
+			//_this.publish(undefined, value);
+
+			reply(null, "okay");
+		};
+		n.rpc_publish = function(reply, time, value) {
+			this.rpc_set(reply, value);
+		};
+		var ltype = type;
+		if (typeof ltype !== "string") {
+			ltype = "schema";
+			schema = type;
+		} else {
+			schema = {
+				"type": type
+			};
+		}
+		n.announce({
+			"type": ltype+".property",
+			"schema": schema
+		});
+
+		return [n, s];
+	};
 
 	this.remotes = {};
 	this.apps = {};
