4ba4579d37279b509571057596a2b24f3a00bbff
Simon Walz <contact@simonwalz.de>
2018-01-31 16:00:03 +0100

    +rpc link

---
+++ er-app-rpc-link.js
@@ -0,0 +1,25 @@
+
+exports.init = function(node, app_config, main, host_info) {
+	var _this = this;
+	
+	node.announce({
+		"type": "link.rpc"
+	});
+
+	if (!node.parentnode) {
+		return;
+	}
+	var sr = node.parentnode.ready("announce", function(method,
+					initial, update) {
+		var _parent = this;
+		if (update) return;
+
+		return _this._source.subscribe(function(initial,
+					do_not_add_to_history){
+			_parent.rpc("set", this.value);
+
+		});
+	});
+
+	return [s, node];
+};
