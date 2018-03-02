ef401b06112b8885a6e191d7db7bae42e83ef4f6
Simon Walz <contact@simonwalz.de>
2018-03-02 19:56:27 +0100

    history: reformated code +purge history function

--- module_history.js
+++ module_history.js
@@ -6,26 +6,33 @@ exports.init = function(router, config) {
 	var History = HG.get_history_module(config);
 
 	router.on("create_new_node", function(node) {
-	    node.ready("announce", function(method, initial, update) {
-		if (update) return;
-		if (node.history) return;
+		node.ready("announce", function(method, initial, update) {
+			if (update) return;
 
-		node.history = new History(node, config);
-
-		node.on("set", function(time, value, only_if_differ, do_not_add_to_history) {
-			// add history:
-			if (typeof do_not_add_to_history === "undefined" || !do_not_add_to_history) {
-				this.history.add(time, value);
-			}
+			exports.init_node(History, router, node, config);
 		});
+	});
+};
 
-		node.subscribe_h = function(callback, timeout, config) {
-			var object = callback.bind(this);
-			if (typeof config !== "object")
-				config = {};
-			config.totime = this.time;
-			var s = this.get_history_on_initialsync(config,
-					function(hdata) {
+exports.init_node = function(History, router, node, config) {
+	if (node.history) return;
+
+	node.history = new History(node, config);
+
+	node.on("set", function(time, value, only_if_differ, do_not_add_to_history) {
+		// add history:
+		if (typeof do_not_add_to_history === "undefined" || !do_not_add_to_history) {
+			this.history.add(time, value);
+		}
+	});
+
+	node.subscribe_h = function(callback, timeout, config) {
+		var object = callback.bind(this);
+		if (typeof config !== "object")
+			config = {};
+		config.totime = this.time;
+		var s = this.get_history_on_initialsync(config,
+			function(hdata) {
 				hdata.forEach(function(d) {
 					object.call(d, true, true);
 				});
@@ -34,40 +41,42 @@ exports.init = function(router, config) {
 				object.remove = s2.remove;
 
 			}, timeout);
-			if (typeof s !== "undefined") {
-				object.remove = s.remove;
-			}
+		if (typeof s !== "undefined") {
+			object.remove = s.remove;
+		}
 
-			return object;
-		};
+		return object;
+	};
 
-		node.get_history = function(config, callback) {
-			this.history.get(config, callback);
-		};
-		node.history.synced = false;
-		node.get_history_on_initialsync = function(config, callback, timeout) {
-			// this = node
+	node.get_history = function(config, callback) {
+		this.history.get(config, callback);
+	};
+	node.history.synced = false;
+	node.get_history_on_initialsync = function(config, callback, timeout) {
+		// this = node
 
-			if (typeof timeout !== "number") timeout = 1000;
-			if (this.connection && !this.history.synced) {
-				var _this = this;
-				return this.once_timeout("history_synced",
-						function(was_timedout) {
+		if (typeof timeout !== "number") timeout = 1000;
+		if (this.connection && !this.history.synced) {
+			var _this = this;
+			return this.once_timeout("history_synced",
+				function(was_timedout) {
 					_this.get_history(config, callback);
 				}, timeout);
-			} else {
-				this.get_history(config, callback);
-				return undefined;
-			}
-		};
+		} else {
+			this.get_history(config, callback);
+			return undefined;
+		}
+	};
 
-		/* register remote procedure calls */
-		node.rpc_history = function(reply, config) {
-			this.get_history(config, function(data) {
-				reply(null, data);
-			});
-		};
-	    });
-	});
+	/* register remote procedure calls */
+	node.rpc_history = function(reply, config) {
+		this.get_history(config, function(data) {
+			reply(null, data);
+		});
+	};
+
+	node.purge_history = function() {
+		node.history = new History(node, config);
+	};
 };
 
