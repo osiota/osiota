4f0f72392935410a13ceac657292658d3b59b4f5
Simon Walz <contact@simonwalz.de>
2018-04-05 16:26:02 +0200

    router: +unique date

--- router.js
+++ router.js
@@ -265,10 +265,11 @@ exports.node.prototype.get_metadata = function() {
 exports.node.prototype.unique_date = function() {
 	var d = new Date()/1000;
 	if (typeof this._unique_date !== "number" ||
-			d - this._unique_date > 0.0005) {
+			d - this._unique_date > 0.00005) {
 		return this._unique_date = d;
 	}
-	return this._unique_date+0.00001;
+	this._unique_date += 0.00001;
+	return this._unique_date;
 
 };
 
