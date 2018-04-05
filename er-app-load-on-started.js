ea9b6395d0bea73698abacda680f7b4a1a8a9cb5
Simon Walz <contact@simonwalz.de>
2018-04-05 18:20:22 +0200

    app: load-on-started

---
+++ er-app-load-on-started.js
@@ -0,0 +1,27 @@
+/*
+ * Usage:
+ * add
+ * 	exports.inherit = ["load-on-started"];
+ * and call your init function:
+ * 	exports.init_delayed(...);
+ */
+
+exports.init = function(node, app_config, main, host_info) {
+	var _this = this;
+
+	var delay = 1;
+	if (typeof app_config.load_delay === "number") {
+		delay = app_config.load_delay;
+	}
+
+	main.on("started", function() {
+		var obj = _this.init_delayed(node, app_config, main, host_info);
+		_this.object_delayed = obj;
+	});
+
+	return tid;
+};
+exports.unload = function(object, unload_object) {
+	unload_object(object);
+	unload_object(this.object_delayed);
+};
