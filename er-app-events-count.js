ca577d1f4fc3a747d9c0be7a6bfa02f8d9e53218
Simon Walz <contact@simonwalz.de>
2018-04-23 18:03:35 +0200

    app: +events-count

---
+++ er-app-events-count.js
@@ -0,0 +1,22 @@
+
+exports.init = function(node, app_config, main) {
+	var count = 0;
+	node.announce({
+		"type": "count.data",
+		"history": false
+	});
+	node.publish(undefined, count);
+
+	var s = this._source.subscribe(function(do_not_add_to_history, initial){
+		if (this.value === null) return;
+		if (do_not_add_to_history && count != 0) {
+			// add one, if there are values not to be added to
+			// history.
+			return;
+		}
+
+		node.publish(this.time, ++count);
+	});
+
+	return [s, node];
+};
