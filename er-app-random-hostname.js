360f92cc3710a9792be83178526a4a585e3bd823
Simon Walz <contact@simonwalz.de>
2018-04-26 09:45:43 +0200

    app: +random hostname

---
+++ er-app-random-hostname.js
@@ -0,0 +1,15 @@
+
+exports.init = function(node, app_config, main) {
+	var prefix = main.router.name + "-";
+	if (typeof app_config.prefix === "string") {
+		prefix = app_config.prefix;
+	}
+
+	//http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+	var s = Math.random().toString(36).substring(2, 15) +
+		Math.random().toString(36).substring(2, 15);
+
+	main.router.name = prefix + s;
+
+	console.log("new hostname:", main.router.name);
+};
