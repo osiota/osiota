438a837431b741a3dc901d584c3c288c3ff3a0f8
Simon Walz <contact@simonwalz.de>
2018-08-11 16:00:31 +0200

    main: web: updated

--- main_web.js
+++ main_web.js
@@ -22,17 +22,28 @@ function main_web(router_name) {
 };
 util.inherits(main_web, main);
 
-main_web.prototype.require = function(appname, callback) {
+main_web.prototype.app_to_path = function(appname) {
+	appname = appname.replace(/^er-app-/, "");
 	if (appname.match(/\//)) {
 		appname += "_web.js";
 	} else {
 		appname += "/web.js";
 	}
-	require_web("apps/" + appname, function(err, object) {
-		//if (err)
 
-		callback(object);	
+	return "node_modules/er-app-" + appname;
+}
+
+main_web.prototype.require = function(appname, callback) {
+	// map app to path:
+	var path = this.app_to_path(appname);
+	this.require_web(path, function(err, object) {
+		//if (err)
+		callback(object);
 	});
+};
+
+main_web.prototype.require_web = function() {
+	console.error("This function has to be overwritten!")
 };
 
 module.exports = main_web;
