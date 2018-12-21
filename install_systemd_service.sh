7354e27dba8ebad08e5af43368c70cd21fe2c91c
Simon Walz <contact@simonwalz.de>
2018-12-21 03:31:54 +0000

    updated systemd install script

--- install_systemd_service.sh
+++ install_systemd_service.sh
@@ -2,16 +2,22 @@
 
 ER_PATH=`pwd`
 ER_CONFIG="config"
-ER_NAME="energy-rotuer"
+ER_NAME="energy-router"
 ER_USER="${USER}"
 
-#if test \! -f "main"
-#then
-#	echo "Run this script in the energy-router repo"
-#	exit 2
-#fi
+if test \! -f "main"
+then
+	echo "Run this script in the energy-router repo"
+	exit 2
+fi
 
-cat <<EOF | sudo "cat >/etc/systemd/system/${ER_NAME}.service"
+if test \! -f "${ER_CONFIG}.json"
+then
+	echo "Config file not found."
+	exit 2
+fi
+
+cat <<EOF | sudo tee /etc/systemd/system/${ER_NAME}.service >/dev/null
 [Unit]
 Description=${ER_NAME}
 After=multi-user.target
