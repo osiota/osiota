50fba8950c91e7db981ebf9f033d6d5765bd7771
Simon Walz <contact@simonwalz.de>
2019-02-11 15:09:38 +0100

    node_map: missing _this variable

--- node_map.js
+++ node_map.js
@@ -31,6 +31,8 @@ exports.node_map = function(node, config, app, map_extra_elements) {
  * Initialize config
  */
 exports.node_map.prototype.init = function() {
+	var _this = this;
+
 	// Map existing config items:
 	this._config.forEach(function(app_config) {
 		var key = _this.map_key(app_config);
