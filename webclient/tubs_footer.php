e7b83916032da94c85bef633ef7ed0a0403cf66d
Simon Walz <contact@simonwalz.de>
2019-09-16 23:11:30 +0200

    deleted old files

--- webclient/tubs_footer.php
+++
@@ -1,5 +0,0 @@
-</div>
-<div style="float: right; font-size: 11px; margin-top: -20px; margin-right: 22px; color: gray;">
-Ansprechpartner: <?php include(dirname(__FILE__)."/ansprechpartner.txt"); ?></div>
-</div>
-</div>
