4b00c0e10cd84a61fcc1cd431c3e7e54a441ca87
Simon Walz <contact@simonwalz.de>
2020-12-12 23:48:28 +0000

    debug: use console.info

--- er-app-debug-show-announce.js
+++ er-app-debug-show-announce.js
@@ -2,7 +2,8 @@
 exports.init = function(node, app_config, main) {
 	var s = this._source.subscribe_announcement(function(
 					node, method, initial, update) {
-		console.log("ANNOUNCE:", method, node.name, update);
+		console.info("ANNOUNCE:", method, node.name, update,
+				node.metadata);
 	});
 	return s;
 }
--- er-app-debug-show-publish.js
+++ er-app-debug-show-publish.js
@@ -4,12 +4,12 @@ exports.init = function(node, app_config, main) {
 					node, method, initial, update) {
 		if (update) return;
 
-		console.log("ANNCOUNCE", method, node.name);
+		console.info("ANNCOUNCE", method, node.name, node.metadata);
 
 		return node.subscribe(function() {
-			console.log("PUBLISH", node.name, node.time, node.value);
+			console.info("PUBLISH", node.name, node.time,
+				node.value);
 		});
-
 	});
 	return s;
 }
