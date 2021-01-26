62da3979e4e2ebaefde8d66ca1f1cc4b6e006229
Simon Walz <contact@simonwalz.de>
2021-01-26 20:56:34 +0100

    tests: added tests

--- test/20_history.js
+++ test/20_history.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-const assert = require('assert').strict;
+var helper = require("./helper_test.js");
+var test = helper.test(__filename);
 
-const osiota = require("../");
-const main = new osiota();
+var osiota = require("../");
+var main = new osiota();
+
 main.config({
 	"app_dir": __dirname+"/",
 	"save_history": false
@@ -17,71 +19,99 @@ for (var i=0; i<10; i++) {
 	n.publish(ds+i, i);
 }
 
-const hc = function(topic, exceeded_value, de_value) {
+const hc = function(t, exceeded_value, de_value) {
+	t.plan(2);
 	return function(data, exceeded) {
-		console.log(topic);
 		var de = data.map(function(d) { return d.value; });
-		assert.equal(exceeded, exceeded_value, "exceeded was not as expected");
-		assert.deepEqual(de, de_value, "data was not as expected");
+		t.equal(exceeded, exceeded_value, "exceeded marker");
+		t.deepEqual(de, de_value, "history data");
 		console.log("history", de, exceeded);
 	};
 };
 
-n.get_history({}, hc("default: --- exceeding", true, [0,1,2,3,4,5,6,7,8,9]));
+test('default --- exceeding', function (t) {
+	n.get_history({}, hc(t, true, [0,1,2,3,4,5,6,7,8,9]));
+});
 
-n.get_history({
-	maxentries: 5
-}, hc("maxentries:", false, [5,6,7,8,9]));
+test('maxentries', function (t) {
+	n.get_history({
+		maxentries: 5
+	}, hc(t, false, [5,6,7,8,9]));
+});
 
-n.get_history({
-	maxentries: 5,
-	reverse_align: false
-}, hc("maxentries:", false, [5,6,7,8,9]));
+test('maxentries', function (t) {
+	n.get_history({
+		maxentries: 5,
+		reverse_align: false
+	}, hc(t, false, [5,6,7,8,9]));
+});
 
-n.get_history({
-	maxentries: 5,
-	reverse_align: true
-}, hc("maxentries:", false, [0,1,2,3,4]));
+test('maxentries', function (t) {
+	n.get_history({
+		maxentries: 5,
+		reverse_align: true
+	}, hc(t, false, [0,1,2,3,4]));
+});
 
-n.get_history({
-	maxentries: 10
-}, hc("maxentries:", false, [0,1,2,3,4,5,6,7,8,9]));
+test('maxentries', function (t) {
+	n.get_history({
+		maxentries: 10
+	}, hc(t, false, [0,1,2,3,4,5,6,7,8,9]));
+});
 
-n.get_history({
-	fromtime: ds+4+0.01
-}, hc("fromtime:", false, [5,6,7,8,9])); // 4 not included.
+test('fromtime', function (t) {
+	n.get_history({
+		fromtime: ds+4+0.01
+	}, hc(t, false, [5,6,7,8,9])); // 4 not included.
+});
 
-n.get_history({
-	fromtime: ds+4
-}, hc("fromtime:", false, [5,6,7,8,9])); // 4 not included.
+test('fromtime', function (t) {
+	n.get_history({
+		fromtime: ds+4
+	}, hc(t, false, [5,6,7,8,9])); // 4 not included.
+});
 
-n.get_history({
-	fromtime: ds+4-0.01
-}, hc("fromtime:", false, [4,5,6,7,8,9])); // 4 included
+test('fromtime', function (t) {
+	n.get_history({
+		fromtime: ds+4-0.01
+	}, hc(t, false, [4,5,6,7,8,9])); // 4 included
+});
 
-n.get_history({
-	totime: ds+5
-}, hc("totime: --- exceeding", true, [0,1,2,3,4])); // 5 not included
+test('totime --- exceeding', function (t) {
+	n.get_history({
+		totime: ds+5
+	}, hc(t, true, [0,1,2,3,4])); // 5 not included
+});
 
-n.get_history({
-	totime: ds+5+0.01
-}, hc("totime: --- exceeding", true, [0,1,2,3,4,5])); // 5 included
+test('totime --- exceeding', function (t) {
+	n.get_history({
+		totime: ds+5+0.01
+	}, hc(t, true, [0,1,2,3,4,5])); // 5 included
+});
 
-n.get_history({
-	fromtime: ds+20
-}, hc("fromtime: future", false, [])); // empty
-
-
-n.get_history({
-	fromtime: ds-5
-}, hc("fromtime (over): --- exceeding", true, [0,1,2,3,4,5,6,7,8,9])); // full
-
-n.get_history({
-	totime: ds-5
-}, hc("totime: --- exceeding", true, [])); // empty
+test('fromtime future', function (t) {
+	n.get_history({
+		fromtime: ds+20
+	}, hc(t, false, [])); // empty
+});
 
 
-n.get_history({
-	fromtime: ds+5,
-	totime: ds+8
-}, hc("from/totime:", false, [6,7])); // range, 5 and 8 not included
+test('fromtime (over) --- exceeding', function (t) {
+	n.get_history({
+		fromtime: ds-5
+	}, hc(t, true, [0,1,2,3,4,5,6,7,8,9])); // full
+});
+
+test('totime --- exceeding', function (t) {
+	n.get_history({
+		totime: ds-5
+	}, hc(t, true, [])); // empty
+});
+
+
+test('from/totime', function (t) {
+	n.get_history({
+		fromtime: ds+5,
+		totime: ds+8
+	}, hc(t, false, [6,7])); // range, 5 and 8 not included
+});
---
+++ test/50_app_execcommand.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var helper = require("./helper_test.js");
+var test = helper.test();
+
+var osiota = require("../");
+var main = new osiota();
+
+main.config({
+	"app": [
+		{
+			"name": "execcommand",
+			"config": {
+				"node": "/say",
+				"command": "echo",
+				"args": [
+					"Hallo"
+				],
+				"map_stdout": true
+			}
+		}
+	]
+});
+
+var n = main.node("/say");
+
+test('app execcommand', function (t) {
+	t.plan(1);
+
+	n.subscribe(function() {
+		if (this.time === null) return;
+		console.log("data:", this.value);
+		t.equal(this.value, "Hallo World", "published value");
+	});
+
+	n.rpc("set", "World");
+});
+
