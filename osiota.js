a1a1b3f804cb839a35505a34c6b650af42bf858d
Simon Walz <contact@simonwalz.de>
2022-01-07 16:51:12 +0100

    feat(cli): use config filename as hostname if not set

--- osiota.js
+++ osiota.js
@@ -130,7 +130,9 @@ if (argv.help && !argv.app) {
 	}
 
 	var config = helper_config_file.read(argv.config);
-	var m = new main(config.hostname || os.hostname());
+	var config_filename = (typeof argv.config === "string" ?
+		argv.config.replace(/^.*\/|\.json$|(osiota|config)[-_]?/g, "") : "");
+	var m = new main(config.hostname || config_filename || os.hostname());
 	m.on("config_save", function() {
 		var _this = this;
 		helper_config_file.write(
