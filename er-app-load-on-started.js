53781b30172e9000613fd93e436a1a8ba17067be
Simon Walz <contact@simonwalz.de>
2022-01-08 21:31:06 +0100

    refactor(load-on-started): run app even if instance was already started

--- er-app-load-on-started.js
+++ er-app-load-on-started.js
@@ -9,15 +9,20 @@
 exports.init = function(node, app_config, main, host_info) {
 	var _this = this;
 
-	var delay = 1;
-	if (typeof app_config.load_delay === "number") {
-		delay = app_config.load_delay;
-	}
-
-	main.on("started", function() {
+	var run_init = function() {
 		var obj = _this.init_delayed(node, app_config, main, host_info);
 		_this.object_delayed = obj;
-	});
+	};
+
+	if (main._started) {
+		run_init();
+		return;
+	}
+	main.once("started", run_init);
+
+	return function() {
+		main.removeListener("started", run_init);
+	};
 };
 exports.unload = function(object, unload_object) {
 	unload_object(object);
