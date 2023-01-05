6760ad11982cef0a89fe167c058a7914bea3f674
Simon Walz <contact@simonwalz.de>
2023-01-05 18:21:22 +0100

    activating tests

--- helper_async_calls.test.js
+++ helper_async_calls.test.js
@@ -1,17 +1,55 @@
 #!/usr/bin/env node
 
+var helper = require("./test/helper_test.js");
+var test = helper.test(__filename);
+
 var async_calls = require("./helper_async_calls.js").async_calls;
 
-async_calls(
-	[
-		function(callback) {
-			setTimeout(callback.bind(null, 1), 500);
-		},
-		function(callback) {
-			setTimeout(callback.bind(null, 2), 1000);
-		},
-	],
-	function(results) {
-		console.log(results);
-	}
-);
+test('async call data', function(t) {
+	t.plan(1);
+	async_calls(
+		[
+			function(callback) {
+				callback(null, 0);
+			},
+			function(callback) {
+				setTimeout(callback.bind(null, null, 1), 50);
+			},
+			function(callback) {
+				setTimeout(callback.bind(null, null, 2), 100);
+			},
+		],
+		function(err, results) {
+			if (err) {
+				console.error(err);
+				t.fail("fetched error");
+				return;
+			}
+			t.deepEqual(results, [0, 1, 2], "data");
+		}
+	);
+});
+
+test('async call error', function(t) {
+	t.plan(1);
+	async_calls(
+		[
+			function(callback) {
+				callback(null, 0);
+			},
+			function(callback) {
+				setTimeout(callback.bind(null, new Error("E1")), 50);
+			},
+			function(callback) {
+				setTimeout(callback.bind(null, new Error("E2")), 10);
+			},
+		],
+		function(err, results) {
+			if (err) {
+				t.deepEqual(err.message, "E2", "data");
+				return;
+			}
+			t.fail("valid data");
+		}
+	);
+});
--- helper_merge_data.test.js
+++ helper_merge_data.test.js
@@ -1,37 +1,44 @@
 #!/usr/bin/env node
 
-const assert = require('assert').strict;
-
-var config = {
-	"app": [{
-		"name": "debug-echo",
-		"config": {
-			"text": "hello world!"
-		}
-	},{
-		"name": "debug-echo",
-		"config": {
-			"text": "hello world!"
-		}
-	}]
-};
-
-var config_new = JSON.parse(JSON.stringify(config));
-
-config_new.app.unshift({"name": "hallowelt"});
-
-//config_new.app.splice(3, 1);
-config_new.app[1].name = "hallo";
-
-config_new.app.push({"name": "hallowelt"});
-config_new.app.push({"name": "hallowelt"});
+var helper = require("./test/helper_test.js");
+var test = helper.test(__filename);
 
 var merge = require("./helper_merge_data.js").merge;
 
-var m = merge(config, config_new);
-console.log("new", config_new.app);
-console.log("NEW", m.app);
+test('merge data', function(t) {
+	t.plan(1);
 
-console.log("equal", JSON.stringify(m) == JSON.stringify(config_new));
+	var config = {
+		"app": [{
+			"name": "debug-echo",
+			"config": {
+				"text": "hello world!"
+			}
+		},{
+			"name": "debug-echo",
+			"config": {
+				"text": "hello world!"
+			}
+		}]
+	};
 
-assert.deepEqual(m, config_new, "config object are not equal");
+	var config_new = JSON.parse(JSON.stringify(config));
+
+	config_new.app.unshift({"name": "hallowelt"});
+
+	//config_new.app.splice(3, 1);
+	config_new.app[1].name = "hallo";
+
+	config_new.app.push({"name": "hallowelt"});
+	config_new.app.push({"name": "hallowelt"});
+
+
+	var m = merge(config, config_new);
+	console.log("new", config_new.app);
+	console.log("NEW", m.app);
+
+	console.log("equal", JSON.stringify(m) == JSON.stringify(config_new));
+
+	t.deepEqual(m, config_new, "config object are not equal");
+
+});
