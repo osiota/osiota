33643c20af4fb530f275eba017d6a94a86b0f0f4
Simon Walz <contact@simonwalz.de>
2023-01-06 12:37:53 +0100

    removing admin apps

--- er-app-admin-test.js
+++
@@ -1,7 +0,0 @@
-
-var load_schema_apps = require("./er-app-admin.js").load_schema_apps;
-
-var s = load_schema_apps(["./", "../", __dirname+"/", __dirname+"/../",
-		__dirname+"/node_modules/"]);
-
-console.log("schema", JSON.stringify(s, undefined, "  "));
